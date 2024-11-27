04532d8b9c3fa459aabf4ebf66fdda85c00c59c0
Iamvijay17 <vijay9578979255@gmail.com>
2024-11-27 22:14:49 +0530

    login form

--- frontend/src/pages/account/signup/index.js
+++ frontend/src/pages/account/signup/index.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import loginFormBg from "../../../assets/loginFormBg.png";
 import { AccountServiceAPI } from "../account.service";
 import SignupForm from "./signUpForm";
 import styles from "./styles.module.css";
+import Loader from "../../../components/loader";
 
 const Signup = () => {
+const [isLoading, setIsLoading] = useState(false);
+
   const handleFinish = (values) => {
+    setIsLoading(true);
     AccountServiceAPI.createAccount(values)
       .then((res) => {
+        setIsLoading(false);
         console.log(res);
       })
       .catch((err) => {
+        setIsLoading(false);
         console.log(err);
       });
   };
@@ -18,6 +24,7 @@ const Signup = () => {
   return (
     <>
       <div className="flex justify-center items-center h-[100%] bg-white">
+      {isLoading && <Loader />}
         <div className="shadow-2xl w-[60%] h-[60%] rounded-2xl grid grid-cols-2 overflow-hidden">
           <div
             className="text-center bg-cover bg-no-repeat h-full flex flex-col items-center px-4"
