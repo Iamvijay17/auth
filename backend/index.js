d5c4de9598c92c6878506f933dc13c63aa5b94a3
Iamvijay17 <vijay9578979255@gmail.com>
2024-12-08 16:53:43 +0530

    product update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import AuthRouter from "./routes/auth.js";
 import userRouter from "./routes/userRoutes.js";
 import swaggerSpec from "./swagger.js";
 
-
+const cors = require('cors');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
