7ff4cc62da4b74f8241ee68d077922982be833bf
Iamvijay17 <vijay9578979255@gmail.com>
2024-12-08 14:15:04 +0530

    product update

--- backend/DB/connect.js
+++ backend/DB/connect.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 export const connectDB = async () => {
+
+    const MONGODB_URI = process.env.NODE_ENV === "production" ? process.env.MONGODB_LIVE_URI : process.env.MONGODB_URI;
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(MONGODB_URI);
+        console.log("Connected to MongoDB" + process.env.NODE_ENV === "production" ? " in production" : " in development");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
