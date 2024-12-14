58dccab22c3bec4081fc0a06cd1d401ff85b563b
Iamvijay17 <vijay9578979255@gmail.com>
2024-12-14 09:11:24 +0530

    service url init

--- backend/DB/connect.js
+++ backend/DB/connect.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 export const connectDB = async () => {
-    const MONGODB_URI = process.env.APP_ENV === "production" ? process.env.MONGODB_URI : process.env.MONGODB_URI;
-    const isProduction = process.env.APP_ENV === "production" ? "in production" : "in development";
-    try {
-        await mongoose.connect(MONGODB_URI);
+  const MONGODB_URI = process.env.MONGODB_URI;
+  const isProduction =
+    process.env.APP_ENV === "production" ? "in production" : "in development";
+  try {
+    await mongoose.connect(MONGODB_URI);
 
-        console.log(`Connected to MongoDB ${isProduction}`);
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+    console.log(`Connected to MongoDB ${isProduction}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
