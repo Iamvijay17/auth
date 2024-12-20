2834184d1ef0dbcac9fa7b1634c5d2ac469d70fd
Iamvijay17 <vijay9578979255@gmail.com>
2024-12-20 22:43:15 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import swaggerSpec from "./swagger.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const version = process.env.API_VERSION || "v1";
 
 // Enable CORS
