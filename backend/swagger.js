89a2b03c5cb9f392abe35ffe3f6baae9769b7951
Iamvijay17 <vijay9578979255@gmail.com>
2024-12-20 21:07:44 +0530

    err fix

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,17 +1,27 @@
 import fs from "fs";
+import yaml from "js-yaml";
+import path from "path";
+// const loadJson = () => {  
+//   try {
+//     const file = fs.readFileSync("./swagger.json", "utf8"); // Adjust path as needed
+//     return JSON.parse(file);
+//   } catch (error) {
+//     console.error("Error loading Swagger JSON file:", error.message);
+//     throw error;
+//   }
+// };
 
-const loadJson = () => {
-
-  
+const loadYaml = () => {
   try {
-    const file = fs.readFileSync("./swagger.json", "utf8"); // Adjust path as needed
-    return JSON.parse(file);
+    const filePath = path.resolve("swagger.yaml");
+    const file = fs.readFileSync(filePath, "utf8");
+    return yaml.load(file);
   } catch (error) {
-    console.error("Error loading Swagger JSON file:", error.message);
+    console.error("Error loading Swagger YAML file:", error.message);
     throw error;
   }
 };
 
-const swaggerSpec = loadJson();
+const swaggerSpec = loadYaml();
 
 export default swaggerSpec;
